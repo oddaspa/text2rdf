892c5feb86e1f9e22f4258bd107bc242f811ba87
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-03-25 10:35:21 +0100

    adding try except

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ import re
 import pandas as pd
 import numpy as np
 from os import path
+from tqdm import tqdm
+import logging, sys
 
 def clean_text(text):
     return re.sub("[^A-Za-z0-9 .!?,]", "", text)
@@ -11,6 +13,7 @@ def get_spo(triple):
     return triple.subject + ", " + triple.relation + ", " + triple.object
 
 if __name__ == "__main__":
+    logging.disable(sys.maxsize)
     te = TripleExtractor()
 
     if not path.exists("corenlp"):
@@ -20,25 +23,25 @@ if __name__ == "__main__":
     train = pd.read_csv("fake-news/train.csv")
     train.drop(train[train['text'].isna()].index, inplace=True)
     train['text'] = train['text'].apply(lambda text: clean_text(text))
+
+    # initialize empty
+    train['triple'] = ["NaN"] * train.shape[0]
     
     extracted_triples = []
-    entities = []
-    for i in range(train.shape[0]):
+    for i in tqdm(range(train.shape[0])):
         tmp_triples = []
-        te.get_doc(train.iloc[i].text)
-        te.getValidTriples()
-        te.set_experimental_relationship()
-        te.set_only_FB15K_valid_triples()
-        te.set_prefered_ner()
-        for triple in te.triples:
-            tmp_triples.append(", ".join([triple.subject, triple.relation, triple.object]))
-            entities.append(triple.subject)
-            entities.append(triple.object)
-        extracted_triples.append(tmp_triples)
+        try:
+            te.get_doc(train.iloc[i].text)
+            te.getValidTriples()
+            te.set_experimental_relationship()
+            te.set_only_FB15K_valid_triples()
+            te.set_prefered_ner()
+            for triple in te.triples:
+                tmp_triples.append([triple.subject, triple.relation, triple.object])
+            if(tmp_triples != []):
+                train.loc[i, "triple"] = [tmp_triples]
     
-    train['triple'] = extracted_triples
-
-    train.to_csv("data_test.csv")
-    entities = np.unique(entities)
-    entities = pd.Series(entities)
-    entities.to_csv("entities.csv")
+            # save each step
+            train.to_csv("data_test.csv")
+        except:
+            print(f"Exception at index {i}.")
